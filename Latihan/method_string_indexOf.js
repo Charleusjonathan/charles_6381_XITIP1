75727a7b1c984ae291a354b34328be0a2a238060
Charleusjonathan <127465280+Charleusjonathan@users.noreply.github.com>
2023-08-08 14:35:12 +0700

    Add files via upload

---
+++ Latihan/method_string_indexOf.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("masukan sebuah kalimat: ", (kalimat) => {
+    rl.question("masukan kata yang ingin anda cari: ", (kataCari) => {
+        //gunakan method indexOf untuk mencari kataCari dalam kalimat
+        const indeksKata = kalimat.indexOf(kataCari);
+        if (indeksKata !== -1) {
+            console.log(`kata '${kataCari}' ditemukan pada indeks: ${indeksKata}`);
+        } else {
+            console.log(`kata '${kataCari}' tidak ditemukan dalam kalimat.`);
+        }
+        rl.close();
+    });
+});
---
+++ Latihan/method_string_trim.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("masukan sebuah kalimat: ", (kalimat) => {
+    // gunakan method trim untuk menghapus spasi di awal dan akhir kalimat
+    const kalimatTrimmed = kalimat.trim();
+    console.log(`kalimat dalam huruf besar: ${kalimatTrimmed}`);
+    rl.close();
+});
